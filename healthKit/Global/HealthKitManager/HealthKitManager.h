fe13bddb6f35b4b4dda08db33e0f8b1083cb69c2
Xihahou <zhangt@jymoit.com>
2016-07-07 18:35:31 +0800

    first commit

---
+++ healthKit/Global/HealthKitManager/HealthKitManager.h
@@ -0,0 +1,13 @@
+//
+//  HealthKitManager.h
+//  healthKit
+//
+//  Created by 张涛 on 16/7/7.
+//  Copyright © 2016年 张涛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HealthKitManager : NSObject
+
+@end
---
+++ healthKit/Global/HealthKitManager/HealthKitManager.m
@@ -0,0 +1,13 @@
+//
+//  HealthKitManager.m
+//  healthKit
+//
+//  Created by 张涛 on 16/7/7.
+//  Copyright © 2016年 张涛. All rights reserved.
+//
+
+#import "HealthKitManager.h"
+
+@implementation HealthKitManager
+
+@end
